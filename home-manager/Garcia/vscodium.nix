f6ba6392aef76ebaf6211b4fabde4084bcf98950
Sam <samcfga@gmail.com>
2025-10-30 16:20:03 -0700

    removing improper vscodium configs

--- home-manager/Garcia/vscodium.nix
+++ home-manager/Garcia/vscodium.nix
@@ -4,21 +4,9 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
-    extensions = with pkgs.vscode-extensions; [
-      # Language support
-      jnoortheen.nix-ide              # Nix language support
-      # ms-python.python              # Python (removed - conflicts with vscodium)
-      # rust-lang.rust-analyzer       # Rust (removed - may conflict)
-
-      # Utilities
-      # eamodio.gitlens               # Git integration (removed - may conflict)
-      # vscodevim.vim                 # Vim keybindings (removed - may conflict)
-
-      # Themes
-      catppuccin.catppuccin-vsc       # Catppuccin theme
-    ];
+    # No extensions - install manually through VSCodium's marketplace
+    extensions = [];
     userSettings = {
-      "workbench.colorTheme" = "Catppuccin Mocha";
       "editor.fontSize" = 14;
       "editor.fontFamily" = "'JetBrains Mono', monospace";
       "editor.tabSize" = 2;
