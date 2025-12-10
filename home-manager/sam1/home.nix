c94a83d46cf19c1258a1602a2ff17f7a4cf8b077
Sam <samcfga@gmail.com>
2025-12-09 16:46:21 -0800

    ...

--- home-manager/sam1/home.nix
+++ home-manager/sam1/home.nix
@@ -1,6 +1,9 @@
 { config, pkgs, ... }:
 
 {
+  # Allow unfree packages in home-manager
+  nixpkgs.config.allowUnfree = true;
+
   # Home Manager needs a bit of information about you and the paths it should manage
   home.username = "sam";
   home.homeDirectory = "/home/sam";
