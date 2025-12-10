e6af0206184098979a6ae9b186507d1ff5053a75
Sam <samcfga@gmail.com>
2025-12-09 16:38:52 -0800

    some hotkeys

--- home-manager/sam1/home.nix
+++ home-manager/sam1/home.nix
@@ -16,6 +16,7 @@
   # User packages
   home.packages = with pkgs; [
     claude-code  # Anthropic's Claude CLI
+    spotify      # Music streaming
   ];
 
   # Basic git configuration
@@ -23,5 +24,54 @@
     enable = true;
     userName = "sam";
     userEmail = "sam@example.com";  # Change this to your email
+  };
+
+  # GNOME keybindings
+  dconf.settings = {
+    # Workspace switching
+    "org/gnome/desktop/wm/keybindings" = {
+      switch-to-workspace-1 = ["<Super>1"];
+      switch-to-workspace-2 = ["<Super>2"];
+      switch-to-workspace-3 = ["<Super>3"];
+      switch-to-workspace-4 = ["<Super>4"];
+    };
+
+    # Custom keybindings list
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      custom-keybindings = [
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/"
+      ];
+    };
+
+    # Super+E: File Manager
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+      name = "Files";
+      command = "nautilus";
+      binding = "<Super>e";
+    };
+
+    # Super+B: Browser
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
+      name = "Browser";
+      command = "firefox";
+      binding = "<Super>b";
+    };
+
+    # Super+V: VSCodium
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2" = {
+      name = "VSCodium";
+      command = "codium";
+      binding = "<Super>v";
+    };
+
+    # Super+M: Spotify
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3" = {
+      name = "Spotify";
+      command = "spotify";
+      binding = "<Super>m";
+    };
   };
 }
